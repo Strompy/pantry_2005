a6d2836581cb6d73cf7c0886c59aa1d07234228f
Phillip Strom <phillip.strom@gmail.com>
2020-06-03 10:30:27 -0600

    Fix restock typo

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -27,7 +27,7 @@ class PantryTest < Minitest::Test
     ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 200})
     pantry.restock(ingredient1, 5)
     pantry.restock(ingredient1, 10)
-    assert_equal 12, pantry.stock_check(ingredient1)
+    assert_equal 15, pantry.stock_check(ingredient1)
     pantry.restock(ingredient2, 7)
     assert_equal 7, pantry.stock_check(ingredient2)
   end
