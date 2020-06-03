9ab8de702f29cf51c60814a5c6d522c2b42f18fd
Phillip Strom <phillip.strom@gmail.com>
2020-06-03 11:07:19 -0600

    Create ingredients test

--- test/cook_book_test.rb
+++ test/cook_book_test.rb
@@ -5,9 +5,27 @@ require './lib/recipe'
 require './lib/cook_book'
 
 class CookBookTest < Minitest::Test
+  def setup
+    @cookbook = CookBook.new
+    @recipe1 = Recipe.new("Mac and Cheese")
+    @recipe2 = Recipe.new("Cheese Burger")
+    @ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
+    @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+    @recipe1.add_ingredient(@ingredient1, 2)
+    @recipe1.add_ingredient(@ingredient2, 8)
+    @ingredient3 = Ingredient.new({name: "Ground Beef", unit: "oz", calories: 100})
+    @ingredient4 = Ingredient.new({name: "Bun", unit: "g", calories: 75})
+    @recipe2 = Recipe.new("Cheese Burger")
+    @recipe2.add_ingredient(@ingredient1, 2)
+    @recipe2.add_ingredient(@ingredient3, 4)
+    @recipe2.add_ingredient(@ingredient4, 1)
+    @cookbook.add_recipe(@recipe1)
+    @cookbook.add_recipe(@recipe2)
+  end
+
   def test_it_exists
-    cookbook = CookBook.new
-    assert_instance_of CookBook, cookbook
+    @cookbook = CookBook.new
+    assert_instance_of CookBook, @cookbook
   end
 
   def test_it_has_attributes
@@ -23,4 +41,10 @@ class CookBookTest < Minitest::Test
     cookbook.add_recipe(recipe2)
     assert_equal [recipe1, recipe2], cookbook.recipes
   end
+
+  def test_all_ingredients
+    expected = ["Cheese", "Macaroni", "Ground Beef", "Bun"]
+    assert_equal expected, @cookbook.ingredients
+  end
+
 end
