d69f9caf74120c87cdf2b0b2577e697a8a8623ab
Phillip Strom <phillip.strom@gmail.com>
2020-06-03 10:54:58 -0600

    Update setup for iteration 3

--- test/recipe_test.rb
+++ test/recipe_test.rb
@@ -8,6 +8,14 @@ class RecipeTest < Minitest::Test
     @recipe1 = Recipe.new("Mac and Cheese")
     @ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
     @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+    @recipe1.add_ingredient(@ingredient1, 2)
+    @recipe1.add_ingredient(@ingredient2, 8)
+    @ingredient3 = Ingredient.new({name: "Ground Beef", unit: "oz", calories: 100})
+    @ingredient4 = Ingredient.new({name: "Bun", unit: "g", calories: 75})
+    @recipe2 = Recipe.new("Cheese Burger")
+    @recipe2.add_ingredient(@ingredient1, 2)
+    @recipe2.add_ingredient(@ingredient3, 4)
+    @recipe2.add_ingredient(@ingredient4, 1)
   end
   def test_it_exists
     assert_instance_of Recipe, @recipe1
@@ -21,14 +29,17 @@ class RecipeTest < Minitest::Test
   end
 
   def test_it_can_add_ingredients
-    @recipe1.add_ingredient(@ingredient1, 2)
-    @recipe1.add_ingredient(@ingredient1, 4)
-    @recipe1.add_ingredient(@ingredient2, 8)
+    recipe1 = Recipe.new("Mac and Cheese")
+    ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
+    ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+    recipe1.add_ingredient(ingredient1, 2)
+    recipe1.add_ingredient(ingredient1, 4)
+    recipe1.add_ingredient(ingredient2, 8)
     expected = {
-      @ingredient1 => 6,
-      @ingredient2 => 8
+      ingredient1 => 6,
+      ingredient2 => 8
     }
-    assert_equal expected, @recipe1.ingredients_required
+    assert_equal expected, recipe1.ingredients_required
   end
 
   def test_it_can_list_ingredients
