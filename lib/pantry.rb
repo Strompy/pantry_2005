8673299169458025abe588e17c73aea7f62a28d0
Phillip Strom <phillip.strom@gmail.com>
2020-06-03 11:42:15 -0600

    Create ingredient in pantry helper method

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -11,4 +11,19 @@ class Pantry
   def restock(ingredient, quantity)
     @stock[ingredient] += quantity
   end
+
+  def ingredient_in_pantry?(recipe)
+    @stock.keys.all? do |ingredient|
+      require "pry"; binding.pry
+      recipe.ingredient_names.include? (ingredient.name)
+    end
+  end
+
+  def enough_ingredient?(recipe)
+    pry
+  end
+
+  def enough_ingredients_for?(recipe)
+    ingredient_in_pantry?(recipe) && enough_ingredient?(recipe)
+  end
 end
