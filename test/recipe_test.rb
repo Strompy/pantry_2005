fec93f15fa9ac727efa40122fa93e0d2d6af8391
Phillip Strom <phillip.strom@gmail.com>
2020-06-03 10:42:08 -0600

    Create ingredients test

--- test/recipe_test.rb
+++ test/recipe_test.rb
@@ -30,4 +30,12 @@ class RecipeTest < Minitest::Test
     }
     assert_equal expected, @recipe1.ingredients_required
   end
+
+  def test_it_can_list_ingredients
+    @recipe1.add_ingredient(@ingredient1, 2)
+    @recipe1.add_ingredient(@ingredient1, 4)
+    @recipe1.add_ingredient(@ingredient2, 8)
+    expected = [@ingredient1, @ingredient]
+    assert_equal expected, @recipe1.ingredients
+  end
 end
